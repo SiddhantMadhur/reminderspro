f49c21f1d52b4f5951f0e5b3a23240d16045b33f
Siddhant Madhur <siddhant.madhur@gmail.com>
2022-09-15 00:43:19 +0400

    added tauri stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Reminders Pro</title>
+        <meta name="description" content="Professional Cross-Platform Reminder and Task taking app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
